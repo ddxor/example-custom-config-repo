3fbc109fca128f788f7aa6b62b55d2f5634e3e07
James Anslow <jamesa@cyber-duck.co.uk>
2021-08-28 18:26:54 +0100

    Typo fix.

--- hooks/aosp_build_pre.sh
+++ hooks/aosp_build_pre.sh
@@ -12,7 +12,7 @@ community_patches_dir="${ROOT_DIR}/community_patches"
 rm -rf "${community_patches_dir}"
 git clone https://github.com/rattlesnakeos/community_patches "${community_patches_dir}"
 patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00001-global-internet-permission-toggle.patch"
-patch -p1 --no-backup-if-mispatch < "${community_patches_dir}/00006-increase-default-maximum-password-length.patch"
+patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00006-increase-default-maximum-password-length.patch"
 
 # apply custom hosts file
 #custom_hosts_file="https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
